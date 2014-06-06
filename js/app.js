0c72ef6d334ef52f02bff301c7e6093c29e3d254
Komarosky Stanislav <facelessone.87@gmail.com>
2014-06-06 14:39:56 +0400

    Some cleanup

--- js/app.js
+++ js/app.js
@@ -465,7 +465,7 @@
             }
         },
         handleGameOver: function () {
-            for (var i = 0, l = this.objs.length, obj, canFall; i < l; ++i) {
+            for (var i = 0, l = this.objs.length, obj; i < l; ++i) {
                 obj = this.objs[i];
                 
                 if (obj.state == this.states.grounded && obj.y <= 0) {
